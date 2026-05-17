6d238469fdaa92e6d040ca1e839355b6be3408ce
jhoancamilorayomejia <jhoan.rayo@correounivalle.edu.co>
2026-05-17 15:29:58 -0500

    framework

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FoundReserves.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
